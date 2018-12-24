d9e39ba7ddaa567b6e888b5268e4b52a5517c20d
student <student@imac-77.local>
2018-12-24 13:38:35 +0800

    Initial Commit

---
+++ SaveMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveMoney
+//
+//  Created by student on 2018/12/24.
+//  Copyright © 2018年 SiCNU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
